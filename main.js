dd2dffa8cfd1050e3b6a117ac5c9fb14466c93e9
testuser <vishnuko200@gmail.com>
2019-03-27 08:27:14 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(255, 0, 255)",
+    "rgb(0, 255, 255)",
+    "rgb(255, 255, 255)"
+]
+var squares = document.querySelectorAll(".square");
+var pickedColor =colors[4];
+var colorDisplay= document.getElementById("colorDisplay");
+var messageDislay = document.getElementById("messageDisplay");
+colorDisplay.innerHTML=pickedColor;
+for(var i=0;i<squares.length;i++)
+{
+    squares[i].style.backgroundColor=colors[i];
+    squares[i].addEventListener("click",function()
+    {
+        var clickedColor= this.style.backgroundColor;
+        if(clickedColor === pickedColor)
+        {
+            messageDislay.innerHTML="Correct";  
+            changColor(clickedColor)
+        }
+        else
+        {
+            this.style.backgroundColor="#232323";
+            messageDislay.innerHTML="Try again";
+        }
+    });
+
+    function changColor(color)
+    {
+        for(var i=0;i<squares.length;i++)
+        squares[i].style.backgroundColor=color;
+    }
+}
