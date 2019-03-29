d5abdcef287a9192cda998d5df44390e8669fdba
testuser <vishnuko200@gmail.com>
2019-03-29 12:42:48 +0530

    third commit

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
- var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(255, 0, 255)",
-    "rgb(0, 255, 255)",
-    "rgb(255, 255, 255)"   
-]
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor =randomColor();
 var colorDisplay= document.getElementById("colorDisplay");
 var messageDislay = document.getElementById("messageDisplay");
+var h1 = document.querySelector("h1");
 colorDisplay.innerHTML=pickedColor;
 for(var i=0;i<squares.length;i++)
 {
@@ -20,7 +14,8 @@ for(var i=0;i<squares.length;i++)
         if(clickedColor === pickedColor)
         {
             messageDislay.innerHTML="Correct";  
-            changColor(clickedColor)
+            changColor(clickedColor);
+            h1.style.backgroundColor=clickedColor;
         }
         else
         {
@@ -30,14 +25,38 @@ for(var i=0;i<squares.length;i++)
     });
 
     function changColor(color)
-    {
+    { 
         for(var i=0;i<squares.length;i++)
         squares[i].style.backgroundColor=color;
+        
+        
     }
 }
 
+//function to  get random color in index
 function randomColor()
 {
     var random = Math.floor(Math.random() * colors.length);
     return colors[random]; 
+}
+
+function generateRandomColors(num)
+{
+    var array =[];
+    for(var i =0;i<num;i++)
+    {
+        array[i] = randomArray();
+    }
+    return array;
+}
+// function to create random color in array
+function randomArray()
+{
+    //select red
+    var red = Math.floor(Math.random() * 256);
+    //select green
+    var green = Math.floor(Math.random() * 256);
+    //select blue
+    var blue = Math.floor(Math.random() * 256);
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
