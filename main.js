b45d01ea65b4e377e45e2e58b01bac9c9229254b
testuser <vishnuko200@gmail.com>
2019-03-29 11:11:56 +0530

    second commit

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-var colors = [
+ var colors = [
     "rgb(255, 0, 0)",
     "rgb(255, 255, 0)",
     "rgb(0, 255, 0)",
     "rgb(255, 0, 255)",
     "rgb(0, 255, 255)",
-    "rgb(255, 255, 255)"
+    "rgb(255, 255, 255)"   
 ]
 var squares = document.querySelectorAll(".square");
-var pickedColor =colors[4];
+var pickedColor =randomColor();
 var colorDisplay= document.getElementById("colorDisplay");
 var messageDislay = document.getElementById("messageDisplay");
 colorDisplay.innerHTML=pickedColor;
@@ -34,4 +34,10 @@ for(var i=0;i<squares.length;i++)
         for(var i=0;i<squares.length;i++)
         squares[i].style.backgroundColor=color;
     }
+}
+
+function randomColor()
+{
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random]; 
 }
