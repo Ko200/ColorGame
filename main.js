2b97d478502e8ad0e4289592fc001b4ce0dc2de6
testuser <vishnuko200@gmail.com>
2019-03-30 07:30:07 +0530

    final commit

--- main.js
+++ main.js
@@ -10,18 +10,20 @@ var pickedColorHard =randomColorHard();
 var colorDisplay= document.getElementById("colorDisplay");
 var messageDislay = document.getElementById("messageDisplay");
 var h1 = document.querySelector("h1");
+var newGame = document.querySelector("#newgame");
 colorDisplay.innerHTML=pickedColor;
 for(var i=0;i<squares.length;i++)
 {
     squares[i].style.backgroundColor=colors[i];
     squares[i].addEventListener("click",function()
-    {
+    {   
         var clickedColor= this.style.backgroundColor;
         if(clickedColor === pickedColor)
-        {
+        {  
             messageDislay.innerHTML="Correct!";  
             changColor(clickedColor);
             h1.style.backgroundColor=clickedColor;
+            
         }
         else
         {
